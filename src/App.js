f9a7006ca18ce543e249de06776d4e79630a185e
tkwedward <tkwedward@gmail.com>
2021-06-17 21:33:46 -0700

    test2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React in less a day! I love Rashida.
+          Learn React in less a day! I love Rashida Very Much!!!
         </a>
       </header>
     </div>
